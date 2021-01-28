280ef482b723cb2e39899408410de60f06dc5a8d
LukeWilder1 <78125054+LukeWilder1@users.noreply.github.com>
2021-01-27 21:09:29 -0500

    Add files via upload

---
+++ hello.java
@@ -0,0 +1,18 @@
+/***************************************
+* Hello World
+**************************
+* Luke Wilder
+* 01/26/2021
+* CMSC 255-003
+****************************/
+
+public class Hello {
+	public static void main(String[] args) {
+		System.out.println("Luke Wilder");
+		System.out.println("Hello World");
+		System.out.println("Hello World");
+		System.out.println("1/26/2021");
+		System.out.println("255-003");
+		System.out.println();
+	}
+}
